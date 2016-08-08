fc6d87250ade5ffac2fd3d53a4e4646cbf971985
Matthew Michaud <mmichaud93@gmail.com>
2016-08-07 20:29:19 -0400

    Add rough tracking and selectable objects

---
+++ Assets/Scripts/SelectableObject.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectableObject : MonoBehaviour {
+    
+    public Rigidbody rigidbody;
+
+    public bool isSelected;
+    public bool isHighlighted;
+
+    public float velocityFactor = 5000f;
+    private Vector3 positionDelta;
+    private float rotationFactor = 400f;
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
+    private Transform interationPoint;
+
+    private TrackedController attachedController;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        interationPoint = new GameObject().transform;
+        isSelected = false;
+
+        velocityFactor /= rigidbody.mass;
+        rotationFactor /= rigidbody.mass;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isSelected && attachedController != null)
+        {
+            positionDelta = attachedController.transform.position - interationPoint.position;
+            Debug.Log(positionDelta);
+            this.rigidbody.velocity = positionDelta * velocityFactor * Time.fixedDeltaTime;
+
+            rotationDelta = attachedController.transform.rotation * Quaternion.Inverse(interationPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+        }
+	}
+
+    public void OnHighlightBegin(TrackedController controller)
+    {
+        Debug.Log("OnHighlightBegin");
+
+        attachedController = controller;
+        isHighlighted = true;
+    }
+
+    public void OnHighlightEnd(TrackedController controller)
+    {
+        Debug.Log("OnHighlightEnd");
+
+        isHighlighted = false;
+    }
+
+    public void OnSelectedBegin(TrackedController controller)
+    {
+        Debug.Log("OnBeginSelected");
+
+        attachedController = controller;
+
+        interationPoint.position = controller.transform.position;
+        interationPoint.rotation = controller.transform.rotation;
+        interationPoint.SetParent(transform, true);
+
+        isSelected = true;
+    }
+
+    public void OnSelectedEnd(TrackedController controller)
+    {
+        if (controller == attachedController)
+        {
+            attachedController = null;
+            isSelected = false;
+        }
+        Debug.Log("OnEndSelected");
+        //controller.ClearSelectedObject();
+        //isSelected = false;
+    }
+}
---
+++ Assets/Scripts/TrackedController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackedController : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    public SteamVR_Controller.Device controller {  get {  return SteamVR_Controller.Input((int) trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    GameObject highlightedObject;
+    GameObject selectedObject;
+
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void Update () {
+	    if (controller != null)
+        {
+            if (controller.GetPressDown(triggerButton))
+            {
+                if (selectedObject != null)
+                {
+                    // drop object
+                    //selectedObject.GetComponent<Rigidbody>().useGravity = true;
+                    selectedObject.GetComponent<SelectableObject>().OnSelectedEnd(this);
+                    selectedObject = null;
+                }
+                else
+                {
+                    // pick up object if one is entered
+                    if (highlightedObject != null)
+                    {
+                        selectedObject = highlightedObject;
+                        if (selectedObject.GetComponent<SelectableObject>().isHighlighted)
+                        {
+                            selectedObject.GetComponent<SelectableObject>().OnHighlightEnd(this);
+                        }
+                        if (selectedObject.GetComponent<SelectableObject>().isSelected) {
+                            selectedObject.GetComponent<SelectableObject>().OnSelectedEnd(this);
+                        }
+                        selectedObject.GetComponent<SelectableObject>().OnSelectedBegin(this);
+                        //selectedObject.GetComponent<Rigidbody>().useGravity = false;
+                        highlightedObject = null;
+                    }
+                }
+
+            }
+        } else
+        {
+            Debug.Log("Controller not initialized");
+        }
+
+        
+
+        if (selectedObject != null)
+        {
+            //selectedObject.transform.position = transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if you dont already have an object selected, highlight an object
+        if (selectedObject == null)
+        {
+            if (other.tag == "SelectableObject")
+            {
+                other.gameObject.GetComponent<SelectableObject>().OnHighlightBegin(this);
+                highlightedObject = other.gameObject;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // if you exit an object and it's the one taht is entered, unenter it
+        if (highlightedObject == other.gameObject) {
+            highlightedObject.GetComponent<SelectableObject>().OnHighlightEnd(this);
+            highlightedObject = null;
+        }
+    }
+
+    public void ClearSelectedObject()
+    {
+        selectedObject = null;
+    }
+}
