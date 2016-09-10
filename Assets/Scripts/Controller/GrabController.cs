35effe5d9424248008624bf8950f5e9fda2ed5cd
Matthew Michaud <mmichaud93@gmail.com>
2016-09-09 21:54:57 -0400

    update the controller code (#48)

---
+++ Assets/Scripts/Controller/GrabController.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabController : MonoBehaviour, TrackedControllerBase.TrackedControllerTriggerListener, TrackedControllerBase.TrackedControllerGripListener
+{
+
+    private TrackedControllerBase trackedControllerBase;
+
+    [HideInInspector]
+    public GameObject selectedObject;
+    [HideInInspector]
+    public bool isHighlighted = false;
+    [HideInInspector]
+    public bool isGrabbed = false;
+    public Vector3 velocity;
+    Vector3 previousPosition = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        trackedControllerBase = GetComponentInParent<TrackedControllerBase>();
+        trackedControllerBase.RegisterTriggerListener(this);
+        trackedControllerBase.RegisterGripListener(this);
+
+        isGrabbed = false;
+        isHighlighted = false;
+    }
+
+    void Update()
+    {
+        if (previousPosition != Vector3.zero)
+        {
+            velocity = (transform.position - previousPosition) / Time.deltaTime;
+        }
+        previousPosition = transform.position;
+    }
+
+    public void OnTriggerPress()
+    {
+
+    }
+    public void OnTriggerPressUp()
+    {
+        if (isGrabbed)
+        {
+            UngrabObject();
+        }
+    }
+    public void OnTriggerPressDown()
+    {
+        if (isHighlighted && !isGrabbed && selectedObject != null)
+        {
+            UnhighlightObject(selectedObject);
+            GrabObject();
+        }
+    }
+
+
+
+
+
+    public void OnGripPress()
+    {
+
+    }
+    public void OnGripPressUp()
+    {
+
+    }
+    public void OnGripPressDown()
+    {
+        if (isGrabbed)
+        {
+            selectedObject.GetComponent<SelectableObject>().Freeze();
+            UngrabObject();
+        }
+    }
+
+
+
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<SelectableObject>() != null)
+        {
+            // can select this one  
+            HighlightObject(collider.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject == selectedObject && isHighlighted)
+        {
+            UnhighlightObject(collider.gameObject);
+        }
+    }
+
+
+
+
+
+    void GrabObject()
+    {
+        isHighlighted = false;
+        isGrabbed = true;
+        selectedObject.GetComponent<SelectableObject>().OnGrab(this);
+    }
+
+    void UngrabObject()
+    {
+        isGrabbed = false;
+        selectedObject.GetComponent<SelectableObject>().OnUngrab(this);
+        selectedObject.GetComponent<Rigidbody>().velocity = velocity;
+        selectedObject = null;
+    }
+
+    void HighlightObject(GameObject gameObject)
+    {
+        selectedObject = gameObject;
+        isHighlighted = true;
+        selectedObject.GetComponent<SelectableObject>().OnHighlight(this);
+    }
+
+    void UnhighlightObject(GameObject gameObject)
+    {
+        isHighlighted = false;
+        selectedObject.GetComponent<SelectableObject>().OnUnhighlight(this);
+    }
+}
---
+++ Assets/Scripts/Controller/TeleportController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportController : MonoBehaviour, TrackedControllerBase.TrackedControllerTrackpadListener {
+
+    private TrackedControllerBase trackedControllerBase;
+
+    // Teleporting feature fields
+    public GameObject teleportReticle;
+    private bool isTeleporting = false;
+    private bool validTeleportSpot = false;
+
+    // Use this for initialization
+    void Start () {
+        trackedControllerBase = GetComponentInParent<TrackedControllerBase>();
+        trackedControllerBase.RegisterTrackpadListener(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnTrackpadTouched(int location) { }
+    public void OnTrackpadPressDown(int location) { }
+
+    public void OnTrackpadPress(int location)
+    {
+        if (location == TrackedControllerBase.TRACKPAD_DOWN)
+        {
+            isTeleporting = true;
+
+            if (!teleportReticle.activeSelf)
+            {
+                teleportReticle.SetActive(true);
+            }
+
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+            RaycastHit floorHit;
+            Debug.DrawRay(transform.position, forward * 2, Color.green);
+            validTeleportSpot = false;
+            if (Physics.Raycast(transform.position, forward, out floorHit, 10))
+            {
+                if (floorHit.collider.gameObject.name == "Floor")
+                {
+                    RaycastHit ceilingHit;
+                    Debug.DrawRay(floorHit.point, Vector3.up * 2, Color.green);
+                    if (!Physics.Raycast(floorHit.point, Vector3.up, out ceilingHit, 1))
+                    {
+                        validTeleportSpot = true;
+                        teleportReticle.transform.position = new Vector3(floorHit.point.x, floorHit.point.y + 0.01f, floorHit.point.z);
+                    }
+                    else
+                    {
+                        teleportReticle.SetActive(false);
+                    }
+                }
+                else
+                {
+                    teleportReticle.SetActive(false);
+                }
+
+            }
+        }
+    }
+
+    public void OnTrackpadPressUp(int location)
+    {
+        if (location == TrackedControllerBase.TRACKPAD_DOWN && isTeleporting && validTeleportSpot)
+        {
+            //teleport there
+            trackedControllerBase.steamVRObject.transform.position = teleportReticle.transform.position;
+            isTeleporting = false;
+            teleportReticle.SetActive(false);
+        }
+    }
+}
