72d082e0583e46d8bc235be47181ef797b714d54
Matthew Michaud <mmichaud93@gmail.com>
2016-10-07 14:05:35 -0400

    Add plebeian camera

---
+++ Assets/TallMatts Playground/PlebeianCamera/PlebeianCameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlebeianCameraController : MonoBehaviour {
+
+    Vector3 previousMousePosition;
+
+	void Update () {
+
+        // zero == left mouse click
+        // one == right mouse click
+        // two == middle mouse click
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePositionDelta = previousMousePosition - Input.mousePosition;
+
+            transform.Rotate(new Vector3(mousePositionDelta.y / 4f, 0, 0));
+            transform.Rotate(new Vector3(0, mousePositionDelta.x / -4f, 0), Space.World);
+            previousMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            previousMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            Vector3 mousePositionDelta = previousMousePosition - Input.mousePosition;
+
+            transform.Translate(mousePositionDelta/100f, Space.Self);
+            previousMousePosition = Input.mousePosition;
+        }
+        transform.Translate(new Vector3(0, 0, Input.mouseScrollDelta.y / 10f), Space.Self);
+    }
+}
