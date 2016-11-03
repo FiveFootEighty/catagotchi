4f97c1471fa5edb33a2c82a163d51b964dca7a47
Chris Murdock <chris.murdock8648@gmail.com>
2016-11-02 21:48:06 -0400

    Basic AI Eventing and Response Infrastructure (#97)
    * WIP: AI Playground, cat AI stuff
    
    * WIP: Building out State machine and decision making infrastructure for CatAI
    
    * WIP: more infrastructure work for AI, still working on this and molding it
    
    * added walk and sitting animations and all the meta files i was ignoring before
    
    * WIP: lots of code changes, revamping of things, etc. some animations stuff too
    
    * WIP: Basic AI eventing management infrastructure

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class AI {
+
+	// internal methods
+	protected abstract void updateActiveState();
+	protected abstract void determineMostPressingEvent();
+	protected abstract void updateTimeDependentVariables(float duration);
+
+
+	// public interface
+	public abstract void update(float duration);
+	//public void addEvent(AIEvent e);
+}
+
+
---
+++ Assets/Scripts/AI/CatStates/RoamingState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RoamingState : ActiveState {
+
+	public RoamingState(CatAI cat) : base(cat) {
+		Debug.Log("Just roaming");
+	}
+
+	public void Update() {
+		base.Update();
+
+		if(this.cat.getBrain().getPriority().eventType.Equals(CatEvent.PET)) {
+			this.changeStates(new CuddlingState(this.cat));
+		}
+	}
+
+}
