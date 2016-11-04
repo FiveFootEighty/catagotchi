e7cfb0fa511ccec94e0a51af08b828a7d2237acc
Matthew Michaud <mmichaud93@gmail.com>
2016-11-04 14:00:22 -0400

    Fix teleport bug (#104)

--- Assets/Scripts/Controller/TeleportController.cs
+++ Assets/Scripts/Controller/TeleportController.cs
@@ -10,12 +10,17 @@ public class TeleportController : MonoBehaviour, TrackedControllerBase.TrackedCo
     private bool isTeleporting = false;
     private bool validTeleportSpot = false;
 
+    private GrabController grabController;
+    public GrabController otherGrabController;
+
     [HideInInspector]
     public GameObject head;
     
     void Start () {
         trackedControllerBase = GetComponentInParent<TrackedControllerBase>();
         trackedControllerBase.RegisterTrackpadListener(this);
+
+        grabController = GetComponent<GrabController>();
 
         head = GameObject.Find("Camera (eye)");
     }
@@ -71,9 +76,114 @@ public class TeleportController : MonoBehaviour, TrackedControllerBase.TrackedCo
             // teleport there
             Vector3 distanceFromCenter = trackedControllerBase.steamVRObject.transform.position - head.transform.position;
             distanceFromCenter.y = 0;
+
+           
+
+            // god I hate this saved Parents thing so much
+            
+           
+            
+
+            /* ----------------- */
             trackedControllerBase.steamVRObject.transform.position = teleportReticle.transform.position + distanceFromCenter;
+            /* ----------------- */
+
+
+            
             isTeleporting = false;
             teleportReticle.SetActive(false);
+        }
+    }
+
+    private ArrayList savedParents = new ArrayList();
+
+    private void LockSelectedObject()
+    {
+        savedParents = new ArrayList();
+
+        if (grabController.isGrabbed && grabController.selectedObject.GetComponent<ContainerBehavior>() != null)
+        {
+            ArrayList containedObjects = grabController.selectedObject.GetComponent<ContainerBehavior>().containedObjects;
+            foreach (Transform containedObject in containedObjects)
+            {
+                if (containedObject != null)
+                {
+                    savedParents.Add(containedObject.parent);
+                    containedObject.parent = trackedControllerBase.steamVRObject.transform;
+                    if (containedObject.GetComponent<Rigidbody>() != null)
+                    {
+                        containedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+            }
+            savedParents.Add(grabController.selectedObject.transform.parent);
+            grabController.selectedObject.transform.parent = trackedControllerBase.steamVRObject.transform;
+            grabController.selectedObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        if (otherGrabController.isGrabbed && otherGrabController.selectedObject.GetComponent<ContainerBehavior>() != null)
+        {
+            ArrayList containedObjects = otherGrabController.selectedObject.GetComponent<ContainerBehavior>().containedObjects;
+            foreach (Transform containedObject in containedObjects)
+            {
+                if (containedObject != null)
+                {
+                    savedParents.Add(containedObject.parent);
+                    containedObject.parent = trackedControllerBase.steamVRObject.transform;
+                    if (containedObject.GetComponent<Rigidbody>() != null)
+                    {
+                        containedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+            }
+            savedParents.Add(otherGrabController.selectedObject.transform.parent);
+            otherGrabController.selectedObject.transform.parent = trackedControllerBase.steamVRObject.transform;
+            otherGrabController.selectedObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    private void UnlockSelectedObject()
+    {
+        int i = 0;
+        if (grabController.isGrabbed)
+        {
+            if (grabController.selectedObject.GetComponent<ContainerBehavior>() != null)
+            {
+                ArrayList containedObjects = grabController.selectedObject.GetComponent<ContainerBehavior>().containedObjects;
+                foreach (Transform containedObject in containedObjects)
+                {
+                    if (containedObject != null)
+                    {
+                        containedObject.parent = (Transform)savedParents[i++];
+                        if (containedObject.GetComponent<Rigidbody>() != null)
+                        {
+                            containedObject.GetComponent<Rigidbody>().isKinematic = false;
+                        }
+                    }
+                }
+                grabController.selectedObject.transform.parent = (Transform)savedParents[i++];
+                grabController.selectedObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+        if (otherGrabController.isGrabbed)
+        {
+            if (otherGrabController.selectedObject.GetComponent<ContainerBehavior>() != null)
+            {
+                ArrayList containedObjects = otherGrabController.selectedObject.GetComponent<ContainerBehavior>().containedObjects;
+                foreach (Transform containedObject in containedObjects)
+                {
+                    if (containedObject != null)
+                    {
+                        containedObject.parent = (Transform)savedParents[i++];
+                        if (containedObject.GetComponent<Rigidbody>() != null)
+                        {
+                            containedObject.GetComponent<Rigidbody>().isKinematic = false;
+                        }
+                    }
+                }
+                otherGrabController.selectedObject.transform.parent = (Transform)savedParents[i++];
+                otherGrabController.selectedObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Items/ContainerBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContainerBehavior : MonoBehaviour {
+
+    [HideInInspector]
+    public ArrayList containedObjects = new ArrayList();
+
+
+    private void AddContainedObject(Transform transform)
+    {
+        foreach (Transform containedObject in containedObjects)
+        {
+            if (transform == containedObject)
+            {
+                return;
+            }
+        }
+        containedObjects.Add(transform);
+    }
+
+    private void RemoveContainedObject(Transform transform)
+    {
+        for (int i = 0; i < containedObjects.Count; i++) {
+            if (transform == (Transform)containedObjects[i])
+            {
+                containedObjects[i] = null;
+                return;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        AddContainedObject(collider.transform);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        RemoveContainedObject(collider.transform);
+    }
+}
