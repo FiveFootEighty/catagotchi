b5e7d0698072aab081c9094072efb9166f8e9d23
Matthew Michaud <mmichaud93@gmail.com>
2016-11-20 19:18:41 -0500

    The cabinet doors can now be slammed

---
+++ Assets/Scripts/Environment/Hinges/LockBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockBehavior : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponentInChildren<HandleObject>() != null)
+        {
+            collider.gameObject.GetComponentInChildren<HandleObject>().LockHinge();
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.GetComponentInChildren<HandleObject>() != null)
+        {
+            collider.gameObject.GetComponentInChildren<HandleObject>().UnlockHinge();
+        }
+    }
+}
